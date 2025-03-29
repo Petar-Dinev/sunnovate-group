ee42142dcc6cd23287db220c1e2989c93f913272
Petar Dinev <p.dinev55@gmail.com>
2025-03-29 09:44:31 +0200

    Added one more section on home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,17 +4,22 @@ import styles from "./Home.module.css";
 export default function Home() {
   return (
     <section className={styles["home-section"]}>
-      <img
-        className={styles["home-page-img"]}
-        src="/home-page-img.jpg"
-        alt="home-page-img"
-      />
-      <section className={styles["home-img-overlay"]}>
-        <h1>Sunnovate Group</h1>
-        <p style={{ fontSize: "1.8em" }}>Innovate. Commit. Empower.</p>
-        <Link className={styles["contract-link"]} to="contacts">
-          CONTRACT US
-        </Link>
+      <section className={styles["main-logo-section"]}>
+        <img className={styles['home-page-logo-img']} src="home-page-logo.jpg" alt="home-page-logo" />
+      </section>
+      <section className={styles["home-contract-section"]}>
+        <img
+          className={styles["home-page-img"]}
+          src="/home-page-img.jpg"
+          alt="home-page-img"
+        />
+        <section className={styles["home-img-overlay"]}>
+          <h1>Sunnovate Group</h1>
+          <p style={{ fontSize: "1.8em" }}>Innovate. Commit. Empower.</p>
+          <Link className={styles["contract-link"]} to="contacts">
+            CONTRACT US
+          </Link>
+        </section>
       </section>
     </section>
   );
