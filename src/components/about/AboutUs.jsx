35ff1967643c51bd80ffada90d898bab1129bbf6
Petar Dinev <p.dinev55@gmail.com>
2025-03-30 10:00:41 +0300

    Refactored about page

--- src/components/about/AboutUs.jsx
+++ src/components/about/AboutUs.jsx
@@ -9,7 +9,25 @@ export default function AboutUs() {
       <header>
         <h2>SUNNOVATE GROUP MANAGEMENT</h2>
       </header>
-      <section className={styles["company-info"]}>
+      <section className={styles['company-info-section']}>
+        <h2>About Sunnovate</h2>
+        <h4>Who We Are</h4>
+        <p>Sunnovate is a leading company in the sector of <strong>renewable energy engineering, procurement, and construction</strong>, specialized in the development and execution of <strong>photovoltaic power plant projects and battery energy storage systems</strong>. With a commitment to <strong>innovation, sustainability, and operational excellence, we provide turnkey solutions and wide flexibility</strong> for project implementations.</p>
+        <h4>Our Mission</h4>
+        <p><em>"Empowering a sustainable future through innovative renewable energy solutions."</em> We strive to accelerate the <strong>global energy transition</strong> by delivering <strong>high-performance solar power systems</strong> that are cost-effective, scalable, and environmentally responsible.</p>
+        <h4>Our Vision</h4>
+        <p>To be a trusted leader in renewable energy, driving innovation and creating lasting value for partners and communities. Committed to sustainability, reliability, and delivering high-quality services with excellence and trust.</p>
+        <h4>Our Core Values</h4>
+        <ul>
+          <li><span></span><strong>Customer-Centric Approach & Commitment</strong> - We tailor solutions to meet our clients' energy needs and business goals.</li>
+          <li><span></span><strong>Integrity</strong> - We build long-term relationships based on transparency, ethics, and trust.</li>
+          <li><span></span><strong>Reliability</strong> - We deliver projects with the highest standards of quality, safety, and precision.</li>
+          <li><span></span><strong>Innovation</strong> - We embrace cutting-edge technology to enhance efficiency and performance.</li>
+          <li><span></span><strong>Sustainability</strong> - We are committed to reducing the world's carbon footprint through clean energy solutions.</li>
+        </ul>
+        <span className={styles['logo-span']}><img src="logo.png" alt="logo-img" /></span>
+      </section>
+      {/* <section className={styles["company-info"]}>
         <section className={styles["info-section"]}>
           <h4>About Sunnovate Group</h4>
           <h5>OUR MISSION</h5>
@@ -31,8 +49,9 @@ export default function AboutUs() {
             alt="sheep"
           />
         </section>
-      </section>
+      </section> */}
       <section className={styles["founder-info"]}>
+        <h2>Our Team</h2>
         <section className={styles["founder-img-section"]}>
           <img
             className={styles["founder-img"]}
