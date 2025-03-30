8c9e1215e40d4b158dde9f8c53ad3b0b087536e1
Petar Dinev <p.dinev55@gmail.com>
2025-03-30 12:41:18 +0300

    Refactored services page

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -6,241 +6,35 @@ export default function Dashboard() {
       <header className={styles["services-header"]}>
         <h3>SERVICES</h3>
       </header>
-      <article className={styles["services-article"]}>
-        <h4>Project Development</h4>
+      <section className={styles["services-info-section"]}>
         <ul>
-          <li>
-            <b>Identification of Investment Opportunities</b>: Pinpointing
-            viable projects in specific countries or regions with high potential
-            for returns and strategic alignment.
-          </li>
-          <li>
-            <b>Strategic Planning</b>: Formulating a clear strategy and
-            preliminary timeline to guide the project's development and
-            execution.
-          </li>
-          <li>
-            <b>Comprehensive Market Research</b>: Conducting in-depth analysis
-            of market conditions, demand, and competition to inform
-            decision-making.
-          </li>
-          <li>
-            <b>Regulatory and Legal Assessment</b>: Evaluating the regulatory
-            environment and legal frameworks to ensure compliance and mitigate
-            risks.
-          </li>
-          <li>
-            <b>Risk Evaluation</b>: Identifying and assessing potential risks
-            and challenges early to develop mitigation strategies.
-          </li>
-          <li>
-            <b>Stakeholder Engagement</b>: Building strong relationships with
-            key stakeholders, including local communities, government
-            authorities, and industry experts, to gain support and address
-            concerns.
-          </li>
-          <li>
-            <b>Feasibility Studies</b>: Conducting technical, economic, and
-            environmental feasibility studies to validate the project’s
-            viability.
-          </li>
-          <li>
-            <b>Financial Modeling</b>: Developing detailed financial projections
-            to ensure the project aligns with organizational goals and funding
-            requirements.
-          </li>
-          <li>
-            <b>Comprehensive Project Plan</b>: Delivering a well-defined roadmap
-            that outlines key milestones, resource requirements, and execution
-            strategies.
-          </li>
-          <li>
-            <b>Alignment with Organizational Goals</b>: Ensuring the project
-            supports broader business objectives and long-term growth
-            strategies.
-          </li>
-          <li>
-            <b>Informed Decision-Making</b>: Providing a solid foundation of
-            data and analysis to enable confident investment and development
-            decisions.
-          </li>
+          <li>End-to-End Expertise &dash; From project design to execution and long-term maintenance.</li>
+          <li>Commitment to Quality & Sustainability &dash; Delivering high-performance, environmentally responsible solutions.</li>
+          <li>Proven Track Record &dash; Successfully executed projects across various scales and regions.</li>
+          <li>Innovation & Technology-Driven Approach &dash; Leveraging the latest advancements in solar and energy storage.</li>
         </ul>
-      </article>
-      <article className={styles["services-article"]}>
-        <h4>Turnkey EPC Solution</h4>
+        <span><img src="logo.png" alt="logo" /></span>
+      </section>
+      <section className={styles['services-content-section']}>
         <ul>
-          <li>
-            <b>Single Point of Responsibility</b>: The EPC contractor assumes
-            full responsibility for the project, reducing the client's
-            administrative burden.
-          </li>
-          <li>
-            <b>Cost and Time Efficiency</b>: Streamlined processes and
-            integrated services minimize delays and cost overruns.
-          </li>
-          <li>
-            <b>Quality Assurance</b>: Ensures high-quality standards through
-            professional engineering, procurement, and construction practices.
-          </li>
-          <li>
-            <b>Risk Mitigation</b>: The contractor manages risks related to
-            design, procurement, and construction, providing the client with a
-            predictable outcome.
-          </li>
-          <li>
-            <b>Scalability</b>: Suitable for projects of all sizes, from
-            small-scale distributed generation to large utility-scale
-            installations.
-          </li>
+          <li>Pre-feasibility and feasibility studies</li>
+          <li>Development support</li>
+          <li>Solar and renewable energy strategies</li>
+          <li>Planning and engineering design of new power plants for stand-alone solar PV, as well as Solar plus energy storage and other hybrid solutions</li>
+          <li>Turnkey EPC services (incl. O&M)</li>
+          <li>Construction works (Mechanical, Electrical, Civil Works)</li>
+          <li>Project management and construction supervision</li>
+          <li>Bidding process & contractual design (EPC/EPCM, O&M, PPA)</li>
+          <li>Turnkey engineering solutions as SCADA, CMMS, LVRT & CCTV</li>
+          <li>Commissioning and certification</li>
+          <li>PPA advisory</li>
+          <li>Operational performance analysis and optimization</li>
+          <li>O&M strategy and OPEX optimization</li>
+          <li>Thermal drone-enabled inspections</li>
+          <li>Repowering studies</li>
+          <li>Capacity building</li>
         </ul>
-        <h4>Applications of Turnkey EPC in PV & BESS projects</h4>
-        <ul>
-          <li>
-            <b>Ulity-Scale Solar Farms</b>: Large-scale PV plants with
-            integrated BESS for grid stability and energy storage.
-          </li>
-          <li>
-            <b>Commercial and Industrial (C&I) Projects</b>: Rooftop or
-            ground-mounted PV systems with BESS for peak shaving, load shifting,
-            and backup power.
-          </li>
-          <li>
-            <b>Microgrids</b>: Off-grid or hybrid systems combining PV and BESS
-            to provide reliable power in remote or underserved areas.
-          </li>
-          <li>
-            <b>Residential Projects</b>: Small-scale PV and BESS installations
-            for homeowners seeking energy independence and cost savings.
-          </li>
-        </ul>
-      </article>
-      <article className={styles["services-article"]}>
-        <h4>Operation and Maintenance</h4>
-        <ul>
-          <li>
-            <b>Maximized System Performance</b>: Regular monitoring and
-            maintenance ensure optimal energy production and storage efficiency,
-            maximizing ROI.
-          </li>
-          <li>
-            <b>Extended Asset Lifespan</b>: Proactive maintenance reduces wear
-            and tear, extending the operational life of PV panels, batteries,
-            and other components.
-          </li>
-          <li>
-            <b>Reduced Downtime</b>: Timely identification and resolution of
-            issues minimize system downtime and ensure consistent energy output.
-          </li>
-          <li>
-            <b>Cost Savings</b>: Preventive maintenance reduces the likelihood
-            of costly repairs and unplanned replacements.
-          </li>
-          <li>
-            <b>Enhanced Safety</b>: Regular inspections and maintenance ensure
-            compliance with safety standards, reducing risks of accidents or
-            system failures.
-          </li>
-          <li>
-            <b>Data-Driven Insights</b>: Advanced monitoring systems provide
-            real-time data and analytics, enabling informed decision-making and
-            performance optimization.
-          </li>
-          <li>
-            <b>Regulatory Compliance</b>: Ensures ongoing adherence to local,
-            national, and international regulations and standards.
-          </li>
-          <li>
-            <b>Warranty Protection</b>: Proper maintenance helps maintain
-            manufacturer warranties, safeguarding investments.
-          </li>
-          <li>
-            <b>Improved Reliability</b>: Ensures the system operates reliably,
-            even under challenging conditions, enhancing energy security.
-          </li>
-          <li>
-            <b>Environmental Benefits</b>: Regular cleaning and upkeep of PV
-            panels and BESS units ensure efficient energy generation and
-            storage, reducing carbon footprints.
-          </li>
-          <li>
-            <b>Stakeholder Confidence</b>: Demonstrates a commitment to quality
-            and reliability, building trust with investors, clients, and
-            stakeholders.
-          </li>
-          <li>
-            <b>Scalability</b>: Supports the expansion of renewable energy
-            systems by maintaining a strong foundation for future growth.
-          </li>
-        </ul>
-      </article>
-      <article className={styles["services-article"]}>
-        <h4>Repowering</h4>
-        <ul>
-          <li>
-            <b>Increased Energy Output</b>: Upgrading to newer, more efficient
-            technologies significantly boosts energy production from existing
-            sites.
-          </li>
-          <li>
-            <b>Improved Efficiency</b>: Modern equipment, such as advanced and
-            high-efficiency PV modules, enhances overall system performance.
-          </li>
-          <li>
-            <b>Cost-Effectiveness</b>: Leveraging existing infrastructure (e.g.,
-            grid connections, land, and permits) reduces capital expenditure
-            compared to building new projects.
-          </li>
-          <li>
-            <b>Extended Project Lifespan</b>: Repowering revitalizes aging
-            assets, extending their operational life and maximizing returns on
-            initial investments.
-          </li>
-          <li>
-            <b>Reduced Maintenance Costs</b>: Newer technologies often require
-            less maintenance and have lower operational costs compared to older
-            systems.
-          </li>
-          <li>
-            <b>Enhanced Reliability</b>: Modern components are more reliable and
-            less prone to failures, ensuring consistent energy generation.
-          </li>
-          <li>
-            <b>Environmental Benefits</b>: Repowering reduces the carbon
-            footprint by replacing outdated systems with cleaner, more efficient
-            technologies.
-          </li>
-          <li>
-            <b>Compliance with Regulations</b>: Ensures adherence to updated
-            regulatory standards and grid codes, avoiding potential penalties or
-            shutdowns.
-          </li>
-          <li>
-            <b>Optimized Land Use</b>: Maximizes energy output from existing
-            sites, reducing the need for additional land acquisition and
-            minimizing environmental impact.
-          </li>
-          <li>
-            <b>Increased Revenue Potential</b>: Higher energy production and
-            improved efficiency lead to greater revenue generation from the same
-            site.
-          </li>
-          <li>
-            <b>Technological Advancements</b>: Incorporates cutting-edge
-            innovations, such as smart grid integration or advanced energy
-            storage solutions.
-          </li>
-          <li>
-            <b>Stakeholder Confidence</b>: Demonstrates a commitment to
-            sustainability and modernization, enhancing trust with investors,
-            communities, and regulators.
-          </li>
-          <li>
-            <b>Grid Stability</b>: Modern systems often provide better grid
-            support, including frequency regulation and voltage control,
-            improving overall grid stability.
-          </li>
-        </ul>
-      </article>
+      </section>
     </section>
   );
 }
